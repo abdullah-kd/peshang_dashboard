c17cef52d66410e934f2245615f04c28c11d6072
abdullakd <abdullasahmad5@gmail.com>
2023-06-27 17:21:03 +0300

    eee

--- peshang_dashboard/settings.py
+++ peshang_dashboard/settings.py
@@ -126,7 +126,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 
 # Default primary key field type
